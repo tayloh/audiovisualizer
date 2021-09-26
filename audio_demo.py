90c9719966f87151c54c76c3ec0cf533dd890ac1
Erik Stolpe <erik.stolpe98@gmail.com>
2021-09-26 02:38:28 +0200

    Add comments for better understanding

--- audio_demo.py
+++ audio_demo.py
@@ -5,33 +5,33 @@ import sys
 import struct
 import matplotlib.pyplot as plt
 import numpy
+import time
 
-if len(sys.argv) < 2:
+if len(sys.argv) < 2 or not sys.argv[1].endswith(".wav"):
     print("Plays a wave file.\n\nUsage: %s filename.wav" % sys.argv[0])
     sys.exit(-1)
 
 wf = wave.open(sys.argv[1], 'rb')
 
-# instantiate PyAudio (1)
+# pyaudio for audio streaming
 p = pyaudio.PyAudio()
 
-
+# get values from wav header
 CHUNK = 1024 * 2 # had to increase chunk size or it started to lag
 FORMAT = p.get_format_from_width(wf.getsampwidth())
 CHANNELS = wf.getnchannels()
 SAMPLERATE = wf.getframerate()
 
 
-# open stream (2)
+# open a stream and set values that we got from wav header
 stream = p.open(format=FORMAT,
                 channels=CHANNELS,
                 rate=SAMPLERATE,
                 output=True)
 
 
-
+# visuals
 fig, (ax, axFFT) = plt.subplots(2)
-x = numpy.arange(0, 2*CHUNK, 2)
 
 # fft stuff
 xFFT = numpy.linspace(0, SAMPLERATE, CHUNK)
@@ -41,6 +41,7 @@ axFFT.set_xlim(20, SAMPLERATE/2)
 axFFT.set_facecolor("k")
 
 # just initiate the plot with CHUNK nr om random y values, will update in while loop
+x = numpy.arange(0, 2*CHUNK, 2)
 line, = ax.plot(x, numpy.random.rand(CHUNK), "g")
 ax.set_ylim(-2**32, 2**32)
 ax.set_xlim(0, CHUNK)
@@ -49,25 +50,63 @@ ax.set_facecolor("k")
 fig.set_facecolor("k")
 fig.show()
 
+nameOfFile = sys.argv[1].split("/")[-1]
+print(f"Playing: {nameOfFile}!")
+
+timeStartPlaying = time.time()
+
 # read data
+# each frame contains 4 bytes of data (2 bytes per channel, since sample resolution is 16 bit)
+# so we get 4 * CHUNK bytes
+# frame:
+#   samples:
+#       channel1: 16 bits
+#       channel2: 16 bits
+# Size of frame is 32 bits or 4 bytes
 data = wf.readframes(CHUNK)
 
-# play stream (3)
-# prevent it from crashing when song ends.. or not
-while len(data) > CHUNK:
-    stream.write(data)
-    data = wf.readframes(CHUNK)
-    dataInt = struct.unpack(str(CHUNK) + "i", data)
+unpack_errors = 0
 
-    # dataInt is an array of ints!
-    line.set_ydata(dataInt)
-    lineFFT.set_ydata(numpy.abs(numpy.fft.fft(dataInt))*2 / (CHUNK * 2**32))
+# So, how many times does this loop run per second? 
+# It gets 2048 frames per loop, so, I guess 44100 / 2048?
+# Let's measure it... 
+# Yup, that checked out.
+# But... how do we know the loop won't run faster or slower than that?
+loopCount = 0
+# send data to audio stream
+while len(data) > 0:
+
+    # write frames to the audio stream to make sound
+    stream.write(data)
+
+    # reads frames, maybe this one reads frames in the speed specified by samplerate?
+    data = wf.readframes(CHUNK)
+
+    # dataInt is an array of ints of size CHUNK!
+    try:
+        dataInt = struct.unpack(str(CHUNK) + "i", data)
+
+        # amplitude graph
+        line.set_ydata(dataInt)
+
+        # fast fourier transform log graph
+        lineFFT.set_ydata(numpy.abs(numpy.fft.fft(dataInt))*2 / (CHUNK * 2**32))
+    except struct.error as e:
+        unpack_errors += 1
+        # print(f"({unpack_errors}) Could not unpack wav data: ", e)
+
+    loopCount += 1
     fig.canvas.draw()
     fig.canvas.flush_events()
 
-# stop stream (4)
+timeStopPlaying = time.time()
+songLengthInSeconds = timeStopPlaying - timeStartPlaying
+
+print("Writes per second:", loopCount / songLengthInSeconds)
+print("Song ended!")
+
+# close the audio stream
 stream.stop_stream()
 stream.close()
 
-# close PyAudio (5)
 p.terminate()
