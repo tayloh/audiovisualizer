d2a6ce0d6a301b3e3930e76883ece5de7709d88f
estolpe <estolpe@kth.se>
2021-09-27 23:45:46 +0200

    Fix wip audio client and pi server streaming lag

--- audio_client.py
+++ audio_client.py
@@ -1,2 +1,121 @@
 # Takes a music file, plays it, does the fft and amplitude calculations. 
 # Sends the results to the pi server.
+
+import pyaudio
+import wave
+import sys
+import struct
+import os
+import socket
+
+############ Check user input ############
+if len(sys.argv) < 2:
+    print("Plays a wave file.\n\nUsage: %s filename.wav" % sys.argv[0])
+    sys.exit(-1)
+
+# find music file in specific folder (only for me right now)
+fileToPlay = ""
+
+musicDirectoryRelative = "../files"
+
+# if / then assume exact path
+if sys.argv[1].find("/") != -1 and sys.argv[1].endswith(".wav"):
+    fileToPlay = sys.argv[1]
+
+# else, look for the song file in the music folder
+else:
+    for file in os.listdir(musicDirectoryRelative):
+        if file.endswith(".wav") and file.find(sys.argv[1]) != -1:
+            fileToPlay = musicDirectoryRelative + "/" + file
+
+try:
+    wf = wave.open(fileToPlay, 'rb')
+except FileNotFoundError:
+    print("File not found.")
+    sys.exit(-1)
+############ Check user input ############
+
+############ Sender ############
+IP = "127.0.0.1"
+PORT = 65432
+
+tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcpSocket.connect((IP, PORT))
+############ Sender ############
+
+############ Play audio and stream data to pi ############
+# pyaudio for audio streaming
+p = pyaudio.PyAudio()
+
+# get values from wav header
+CHUNK = 3 * 1024 # (2* 1024) had to increase chunk size or it started to lag
+FORMAT = p.get_format_from_width(wf.getsampwidth())
+CHANNELS = wf.getnchannels()
+SAMPLERATE = wf.getframerate()
+
+
+# open a stream and set values that we got from wav header
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=SAMPLERATE,
+                output=True)
+
+
+nameOfFile = fileToPlay.split("/")[-1]
+print(f"Playing: {nameOfFile}!")
+
+
+# read data
+# each frame contains 4 bytes of data (2 bytes per channel, since sample resolution is 16 bit)
+# so we get 4 * CHUNK bytes
+# frame:
+#   samples:
+#       channel1: 16 bits
+#       channel2: 16 bits
+# Size of frame is 32 bits or 4 bytes
+data = wf.readframes(CHUNK)
+
+# So, how many times does this loop run per second? 
+# It gets 2048 frames per loop, so, I guess 44100 / 2048?
+# Let's measure it... 
+# Yup, that checked out.
+# But... how do we know the loop won't run faster or slower than that?
+
+# send data to audio stream
+while len(data) > 0:
+
+    # send the data before playing it...
+    tcpSocket.sendall(data)
+
+    # wait for a response before playing the data - hacky solution?
+    #tcpSocket.recv(256)
+
+    # write frames to the audio stream to make sound
+    stream.write(data)
+
+    # reads frames, maybe this one reads frames in the speed specified by samplerate?
+    data = wf.readframes(CHUNK)
+
+    # dataInt is an array of ints of size CHUNK!
+    try:
+        dataInt = struct.unpack(str(CHUNK) + "i", data)
+
+    except struct.error as e:
+        print("Could not unpack wav data: ", e)
+
+############ Play audio and stream data to pi ############
+
+############ Shutdown ############
+print("Song ended!")
+
+# close tcp socket
+tcpSocket.close()
+
+# close the audio stream
+stream.stop_stream()
+stream.close()
+
+p.terminate()
+
+############ Shutdown ############
+
--- pi_server.py
+++ pi_server.py
@@ -2,26 +2,74 @@
 
 import socket
 import struct
+import numpy as np
+import matplotlib.pyplot as plt
 
+CHUNK = 3 * 1024 # this had to be fine tuned to work with: gui update delay - stream delay - music playing
+
+############ Visuals ############ (temp, TODO: led strip)
+fig, (ax, axFFT) = plt.subplots(2)
+
+# should not need to hardcode this mby
+SAMPLERATE = 44100
+
+# fft stuff
+xFFT = np.linspace(0, SAMPLERATE, CHUNK)
+lineFFT, = axFFT.semilogx(xFFT, np.random.rand(CHUNK), "tab:purple")
+axFFT.set_ylim(0, 1)
+axFFT.set_xlim(20, SAMPLERATE/2)
+axFFT.set_facecolor("k")
+
+# just initiate the plot with CHUNK nr on random y values, will update in while loop later
+x = np.arange(0, 2*CHUNK, 2)
+line, = ax.plot(x, np.random.rand(CHUNK), "g")
+ax.set_ylim(-2**32, 2**32)
+ax.set_xlim(0, CHUNK)
+ax.set_facecolor("k")
+
+fig.set_facecolor("k")
+fig.show()
+############ Visuals ############
+
+############ Listener ############
 IP = "127.0.0.1"
 PORT = 65432
-CHUNK = 1024 * 2
 BUFFER_SIZE = CHUNK * 4 # size of each chunk of audioframes in bytes
 
 tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcpSocket.bind((IP, PORT))
 
 tcpSocket.listen()
-connection, sourceAddr = tcpSocket.accept()
-
-print("Connection from:", sourceAddr)
+tcpSocket.settimeout(10) # seconds
+print("Listening on:", (IP, PORT))
 
 while True:
-    data = connection.recv(BUFFER_SIZE)
-    if not data:
-        break
-    dataInt = struct.unpack(str(CHUNK) + "i", data)
-    print(sum(dataInt)/len(dataInt))
+    connection, sourceAddr = tcpSocket.accept()
 
-connection.close()
+    print("Connection from:", sourceAddr)
+
+    while True:
+        data = connection.recv(BUFFER_SIZE)
+        if not data:
+            break
+        try:
+            dataInt = struct.unpack(str(CHUNK) + "i", data)
+            #connection.send(b"any")
+
+            # amplitude graph
+            line.set_ydata(dataInt)
+
+            # fast fourier transform log graph
+            lineFFT.set_ydata(np.abs(np.fft.fft(dataInt))*2 / (CHUNK * 2**32))
+
+        except struct.error:
+            print("Unpack error.")
+    
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+
+    connection.close()
+
 tcpSocket.close()
+
+############ Listener ############
